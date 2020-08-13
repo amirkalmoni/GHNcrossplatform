ee159a610b4a47f46902477eecdba42ed9623a79
Amir Kalmoni <amirkalmoni@gmail.com>
2020-08-13 18:53:11 -0400

    * google-services.json: Json file to link Andoid version of app to   firebase database
    * packages.config:
    
    * MainActivity.cs: connection tested and new instance of database
      created with connection tested

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,13 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Firebase;
+using Firebase.Database;
 
 namespace GHNcrossplatform.Droid
 {
     [Activity(Label = "GHN", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        Button buttontestconnection;
+        FirebaseDatabase database;
+        
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,10 +22,53 @@ namespace GHNcrossplatform.Droid
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            buttontestconnection = FindViewById<Button>(Resource.Id.myButton);
+            buttontestconnection.Click += Buttontestconnection_Click;
+            
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            
         }
+
+        private void Buttontestconnection_Click(object sender, System.EventArgs e)
+        {
+            InitializeDatabase();
+        }
+
+        //function to initialize the database
+        void InitializeDatabase() 
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            //condition is to force initialization incase it returns null
+            if (app == null) 
+            {
+                var options = new FirebaseOptions.Builder()
+                .SetApplicationId("ghn - authentication")
+                .SetApiKey("AIzaSyCibbH9NC4NZ6ieNSiM8-TTDhR34N76pTM")
+                .SetDatabaseUrl("https://ghn-authentication.firebaseio.com")
+                .SetStorageBucket("ghn-authentication.appspot.com")
+                .Build();
+
+
+                //Initialization of database in app
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+
+            }
+            //Code Continues if itialization doesnt fail
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("Test");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "it Works bruv", ToastLength.Short).Show();
+
+        }
+
     }
+
 }
 
